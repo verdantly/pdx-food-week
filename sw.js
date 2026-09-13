ffaad0616629d3df2d9553f405eee80524064533
verdantly <a.verdantly@gmail.com>
2026-09-13 11:05:12 -0700

    fix(mobile): restore #header-meta in mobile viewport by targeting switcher specifically

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'pdxfw-cache-mu04afc0';
+const CACHE_NAME = 'pdxfw-cache-mu04h2b2';
 
 const STATIC_ASSETS = [
   './',
   'index.html',
-  'css/style.css?v=mu04afc0',
+  'css/style.css?v=mu04h2b2',
   'js/app.js?v=mu044r7p',
   'js/meta.js?v=mu044r7p',
   'js/modules/auth.js',
