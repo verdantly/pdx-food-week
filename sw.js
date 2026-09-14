45a9cf69173d8ae6cc24b70d1a56278d6e341b4f
verdantly <a.verdantly@gmail.com>
2026-09-13 20:07:58 -0700

    style(landing): add bottom padding to .landing-others-column on tablet viewports

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'pdxfw-cache-773f7256';
+const CACHE_NAME = 'pdxfw-cache-5744e818';
 
 const STATIC_ASSETS = [
   './',
   'index.html',
-  'css/style.css?v=773f7256',
-  'js/app.js?v=773f7256',
-  'js/meta.js?v=773f7256',
+  'css/style.css?v=5744e818',
+  'js/app.js?v=5744e818',
+  'js/meta.js?v=5744e818',
   'js/modules/auth.js',
   'js/modules/cards.js',
   'js/modules/crawl.js',
